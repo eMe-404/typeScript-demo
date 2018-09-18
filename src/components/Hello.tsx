40b7a9feacbeb37961b1952ae8995e0bba1eac7e
eMe-404 <838702939>
2018-09-18 21:39:42 +0800

    [yaer] refactor : change stateless function to class component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,18 +5,24 @@ export interface Props {
     enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-    if (enthusiasmLevel <= 0) {
-        throw new Error('You could be a little more enthusiastic. :D');
-    }
+class Hello extends React.Component<Props, Object> {
+    //<Props, Object> are the arguments we passing to the component 
+    //Props is this.props and Object is this.state
 
-    return (
-        <div className="hello">
-            <div className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
+    render() {
+        const { name, enthusiasmLevel = 1 } = this.props
+        if (enthusiasmLevel <= 0) {
+            throw new Error('You could be a little more enthusiastic. :D');
+        }
+
+        return (
+            <div className="hello">
+                <div className="greeting">
+                    Hello {name + getExclamationMarks(enthusiasmLevel)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Hello;
