3b6e6083b964fd5afc4f6b15270d7a38f78a1afb
eMe-404 <838702939@qq.com>
2018-09-21 01:01:27 +0800

    [yaer] refactor : Change Hello component to stateless

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,46 +4,26 @@ import './Hello.css'
 export interface Props {
     name: string;
     enthusiasmLevel?: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-interface State {
-    currentEnthusiasm: number;
-}
-class Hello extends React.Component<Props, State> {
-    //<Props, State> are the arguments we passing to the component 
-    //Props is this.props and State is this.state
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            currentEnthusiasm: props.enthusiasmLevel || 1
-        }
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+    if (enthusiasmLevel <= 0) {
+        throw new Error('You could be a little more enthusiastic. :D');
     }
 
-    onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1)
-    onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1)
-
-    updateEnthusiasm = (currentEnthusiasm: number) => {
-        this.setState({
-            currentEnthusiasm
-        })
-    }
-
-    render() {
-        const { name } = this.props
-        if (this.state.currentEnthusiasm <= 0) {
-            throw new Error('You could be a little more enthusiastic. :D');
-        }
-
-        return (
-            <div className="hello">
-                <div className="greeting">
-                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
-                </div>
-                <button onClick={this.onIncrement}>+</button>
-                <button onClick={this.onDecrement}>-</button>
+    return (
+        <div className="hello">
+            <div className="greeting">
+                Hello {name + getExclamationMarks(enthusiasmLevel)}
             </div>
-        );
-    }
+            <div>
+                <button onClick={onDecrement}>-</button>
+                <button onClick={onIncrement}>+</button>
+            </div>
+        </div>
+    );
 }
 
 export default Hello;
